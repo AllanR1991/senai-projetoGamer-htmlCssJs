a170ba1afdbe989aa5e677ed0528d4764c45e8a4
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2023-03-22 19:11:27 -0300

    Realizado o header versão mobile.

---
+++ js/exibe_menu.js
@@ -0,0 +1,6 @@
+function exibeMenu() {
+    document.getElementById("menu-nav").classList.toggle("exibe-menu-nav");
+}
+function exibeMenuOpcoes() {
+    document.getElementById("menu-opcoes").classList.toggle("exibe-menu-opcoes");
+}
