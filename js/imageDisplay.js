30585159d013192b40e3f430bb2101516ee6f204
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2023-03-28 22:13:59 -0300

    Inciado secao cadastro equipe, introduzido input de foto.

---
+++ js/imageDisplay.js
@@ -0,0 +1,61 @@
+const input = document.getElementById('image_uploads');
+const preview = document.querySelector('.preview');
+
+// input.style.opacity = 0;
+
+input.addEventListener('change', updateImageDisplay);
+
+function updateImageDisplay() {
+    while(preview.firstChild) {
+      preview.removeChild(preview.firstChild);
+    }
+  
+    const curFiles = input.files;
+    if (curFiles.length === 0) {
+      const para = document.createElement('p');
+      para.textContent = 'No files currently selected for upload';
+      preview.appendChild(para);
+    } else {
+      const list = document.createElement('ol');
+      preview.appendChild(list);
+  
+      for (const file of curFiles) {
+        const listItem = document.createElement('li');
+        const para = document.createElement('p');
+        if (validFileType(file)) {
+          para.textContent = `File name ${file.name}, file size ${returnFileSize(file.size)}.`;
+          const image = document.createElement('img');
+          image.src = URL.createObjectURL(file);
+  
+          listItem.appendChild(image);
+          listItem.appendChild(para);
+        } else {
+          para.textContent = `File name ${file.name}: Not a valid file type. Update your selection.`;
+          listItem.appendChild(para);
+        }
+  
+        list.appendChild(listItem);
+      }
+    }
+  }
+
+
+  const fileTypes = [
+    "image/jpeg",
+    "image/png",
+    "image/svg+xml",
+  ];
+  
+  function validFileType(file) {
+    return fileTypes.includes(file.type);
+  }
+
+  function returnFileSize(number) {
+    if (number < 1024) {
+      return `${number} bytes`;
+    } else if (number >= 1024 && number < 1048576) {
+      return `${(number / 1024).toFixed(1)} KB`;
+    } else if (number >= 1048576) {
+      return `${(number / 1048576).toFixed(1)} MB`;
+    }
+  }
